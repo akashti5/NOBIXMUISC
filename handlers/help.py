46d4b93a85e71692e453f393770b11019d6a42c0
akashti5 <78689979+akashti5@users.noreply.github.com>
2021-06-04 18:42:00 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -5,13 +5,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**Contact me in PM to get help of possible commands ⁉️**""",
+      await message.reply_text("""**𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐌𝐄 𝐈𝐍 𝐏𝐌 𝐓𝐎 𝐆𝐄𝐓 𝐇𝐄𝐋𝐏 𝐎𝐅 𝐏𝐎𝐒𝐒𝐈𝐁𝐋𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒 ⁉️**""",
       reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton(
                                 text="Help",
-                                url="t.me/Music_op_bot?start=ghelp_{}".format(
+                                url="t.me/NoBi_vC_PlAyEr_RoBoT?start=ghelp_{}".format(
                                     context.bot.username, module,
                                 ),
                             ),
